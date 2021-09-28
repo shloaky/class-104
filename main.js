dc578d23cdcb53e63d37c9f9f32159ac8bb2f52b
shloaky <77309360+shloaky@users.noreply.github.com>
2021-09-27 20:36:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+ 
+cam = document.getElementById("webcam");
+Webcam.attach("#webcam");
+
+function takeasnapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="photo" src = "'+data_uri+'"/>';   
+    });
+}
+
+console.log('ml5 version: ',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Cz4Ja5Y1R/modle.json',modelloaded);
+function modelloaded(){
+    console.log('modelload!');
+}
